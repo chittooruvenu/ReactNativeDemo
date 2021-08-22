51c26be1b4cb8b51a8f297d222c4ddc0bdef5884
chittooruvenu <chittooru.venu@gmail.com>
2021-08-22 12:00:52 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1> This is first React Native Application</h1>
+      <h2>Start editing to see some magic happen!</h2>
+    </div>
+  );
+}
